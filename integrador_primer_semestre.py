99cf971b21d48bb7a9393b645ad3cfda4177b6ca
brissa596 <munozbrisa856@gmail.com>
2025-10-22 10:59:53 -0300

    Update integrador_primer_semestre.py

--- integrador_primer_semestre.py
+++ integrador_primer_semestre.py
@@ -141,6 +141,56 @@ def ordenar_paises_por_superficie(lista_paises: list):
                 lista_paises[j]= aux
     return lista_paises
 
+ 
+#esta funcion calcula y muestra las estadisticas  
+
+def mostrar_estadisticas(lista_paises:list) :
+  if not lista_paises:
+    print("no hay paises cargados")
+    return 
+  
+  #inicializamos variables para la mayor y menor poblacion 
+
+  pais_mayor_pob= lista_paises[0]
+  pais_menor_pob = lista_paises[0]
+  total_poblacion= 0
+  total_superficie=0
+
+    #Recorremos la lista
+  for p in lista_paises:
+    total_poblacion+= p.poblacion 
+    total_superficie+= p.superficie
+
+
+    if p.poblacion > pais_mayor_pob.poblacion:
+      pais_mayor_pob = p
+
+    if p.poblacion < pais_menor_pob.poblacion :
+      pais_menor_pob= p
+       
+
+  promedio_pob = total_poblacion / len(lista_paises)
+  promedio_sup = total_superficie / len(lista_paises)
+
+
+    #Contar países por continente
+  paises_por_continente = {}
+  for p in lista_paises: 
+        cont = p.continente
+        if cont not in paises_por_continente:
+            paises_por_continente[cont] = 1
+        else:
+            paises_por_continente[cont] += 1
+
+  print("\n ESTADÍSTICAS GENERALES ")
+  print(f"➡ País con mayor población: {pais_mayor_pob.nombre} ({pais_mayor_pob.poblacion:,} hab.)")
+  print(f"➡ País con menor población: {pais_menor_pob.nombre} ({pais_menor_pob.poblacion:,} hab.)")
+  print(f"➡ Promedio de población: {promedio_pob:,.0f}")
+  print(f"➡ Promedio de superficie: {promedio_sup:,.0f} km²")
+  print("➡ Cantidad de países por continente:")
+  for cont, cant in paises_por_continente.items():
+        print(f"   - {cont}: {cant}")
+
 
 
 #Estructura Principal del programa.
@@ -158,6 +208,7 @@ if __name__ == "__main__":
             print("1. Mostrar todos los países")
             print("2. Buscar por coincidencia o busquedar parcial")
             print("3. Filtrar por continente")
+            print("4. Mostrar estadisticas")
             print("0. Salir")
 
             opcion = input("Elija una opción: ").strip()
@@ -185,7 +236,10 @@ if __name__ == "__main__":
                     mostrar_todos_los_paises(lista_ordenada)
                 elif otra_opcion == "0":
                     print("Volviendo al menu principal...")
-
+                    
+            elif opcion == "4":
+                estadistica= mostrar_estadisticas(lista_paises)
+                
             elif opcion == "0":
                 print("Saliendo del programa...")
                 break
