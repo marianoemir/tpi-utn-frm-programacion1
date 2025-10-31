277d242edb76964e12bc3d3e389cdd3213a5c2b9
Mariano Chirino <marianochirino95@gmail.com>
2025-10-31 18:17:42 -0300

    comenatrios y readme
    comentarios mas cortos  y readme completo.

--- README.md
+++ README.md
@@ -1,2 +1,251 @@
-# tpi-utn-frm-programacion1
-Aplicación de Gestión de Información sobre Paises
+### Descripción del Proyecto:
+
+Este proyecto es un programa desarrollado en Python que permite gestionar información de países mediante la lectura y escritura de un archivo CSV.
+El sistema aplica conceptos de Programación Orientada a Objetos (POO) y estructuras de datos como listas y diccionarios, ofreciendo un menú interactivo para que el usuario pueda:
+
+Mostrar, buscar, ordenar y filtrar países.
+
+Calcular estadísticas generales.
+
+Agregar nuevos países con validaciones automáticas.
+
+El trabajo integra los contenidos vistos en la materia, aplicando la lógica de programación, validación de datos, uso de clases, métodos y manejo de archivos.
+
+### Datos institucionales
+
+Universidad: Universidad Tecnológica Nacional (UTN).
+Facultad Regional: [Facultad Regional Mendoza (UTN FRM)].
+Carrera: Tecnicatura Universitaria en Programación.
+Asignatura: Programación I.
+Año lectivo: 2025 – 2° Cuatrimestre.
+Comisión: 1.
+Docente: Magni Negri, Gerardo Luis.
+
+Integrantes del grupo:
+
+Mariano Chirino.
+
+
+
+### Estructura:
+
+El proyecto se desarrolló en equipo, organizando el trabajo por etapas y dividiendo las responsabilidades entre los integrantes.
+Cada miembro del grupo trabajó en su propia rama de desarrollo, aportando funciones específicas como la lectura del archivo CSV, la creación del menú principal, la búsqueda de países, los filtros, las estadísticas y la funcionalidad de agregar nuevos registros.
+
+Una vez completadas las partes individuales, se realizó la integración final en un único archivo Python, verificando el correcto funcionamiento general y la coherencia entre las validaciones, el menú y las estructuras de datos.
+
+### Instrucciones de ejecución:
+
+Para ejecutar el programa correctamente:
+
+Descargar o clonar la carpeta del proyecto completa, asegurándose de que el archivo
+integrador_primer_semestre.py y paises.csv estén en la misma carpeta.
+
+Opción 1 – Desde Visual Studio Code (VS Code):
+
+Abrir la carpeta del proyecto en VS Code.
+
+Seleccionar el archivo integrador_primer_semestre.py.
+
+Presionar el botón “Run Python File” (ícono ▶️ en la esquina superior derecha).
+
+El menú principal aparecerá en la consola integrada.
+
+Opción 2 – Windows (CMD Y POWERSHELL)
+
+Para ejecutar el programa correctamente, asegurarse de tener instalada una versión de Python 3.10 o superior y de que el archivo paises.csv se encuentre en la misma carpeta que integrador_primer_semestre.py.
+
+1.Windows (CMD)
+
+Abrir el Símbolo del sistema (CMD).
+
+Navegar hasta la carpeta donde se encuentra el proyecto:
+
+cd Ruta\hasta\la\carpeta\del\proyecto
+
+Ejecutar el programa:
+
+python integrador_primer_semestre.py
+
+2. Windows (PowerShell)
+
+Abrir PowerShell.
+
+Cambiar al directorio del proyecto:
+
+cd "C:\Ruta\hasta\la\carpeta\del\proyecto"
+
+Ejecutar el programa:
+python .\integrador_primer_semestre.py
+
+Si ese comando no funcionara:
+
+py .\integrador_primer_semestre.py
+
+
+Opción 3 – Linux (Ubuntu, Debian, etc.)
+
+Abrir una terminal.
+
+Ir al directorio del proyecto:
+
+cd /ruta/hasta/la/carpeta/proyecto
+
+Ejecutar el programa:
+python3 integrador_primer_semestre.py
+
+4. macOS –
+
+Abrir la Terminal.
+
+Navegar a la carpeta donde está el archivo:
+
+cd /Users/tu_usuario/Documentos/proyecto
+
+Ejecutar el programa:
+
+python3 integrador_primer_semestre.py
+
+Recomendaciones generales
+
+Asegurarse de que el archivo paises.csv esté en la misma carpeta que el programa.
+
+En caso de error “python no reconocido”, verificar que Python esté instalado y agregado al PATH.
+
+Para salir del programa, seleccionar la opción 0 en el menú principal.
+
+### Librerias:
+
+import csv 
+csv es parte de la librería estándar de Python, cualquier persona que tenga Python instalado (de la versión correcta) podrá ejecutar el proyecto sin necesidad de instalar nada adicional.
+
+La versión mínima de Python que se necesita, por ejemplo: Python 3.10+.
+
+Link a repositorio de GitHub: https://github.com/marianoemir/tpi-utn-frm-programacion1  
+Link a Video del Proyecto: 
+
+
+### Ejemplos de entrada y salida:
+
+Archivo de entrada (paises.csv)
+
+codigo,nombre,poblacion,superficie,continente
+1,Argentina,45376763,2780400,América
+2,Brasil,213993437,8515767,América
+3,Alemania,83149300,357022,Europa
+4,Egipto,104000000,1002450,África
+5,Australia,25690000,7692000,Oceanía
+6,China,1412000000,9596960,Asia
+7,India,1393000000,3287263,Asia
+8,Canadá,38000000,9984670,América
+9,España,47450795,505990,Europa
+10,Argelia,47400000,2381741,África
+
+1. Búsqueda parcial de país
+
+=== MENÚ PRINCIPAL ===
+2
+=== BUSCAR PAÍS POR NOMBRE ===
+1. Búsqueda exacta (nombre completo)
+2. Búsqueda parcial (parte del nombre)
+0. Volver al menú principal
+Elija una opción: 2 
+Ingrese parte del nombre del país: an
+
+Se encontraron 2 coincidencias:
+3 - Alemania | Pob: 83,149,300 | Sup: 357,022 km² | Europa
+8 - Canadá | Pob: 38,000,000 | Sup: 9,984,670 km² | América
+
+
+2. Agregar un nuevo país
+
+Entrada:
+
+=== MENÚ PRINCIPAL ===
+6
+=== AGREGAR NUEVO PAÍS ===
+(Escriba 0 y ENTER para cancelar en cualquier paso)
+Nombre del país: Chile
+Población (solo números): 19107216
+Superficie en km² (solo números): 756102
+Continente: América
+
+Salida:
+País 'Chile' agregado con éxito (código: 11)
+
+El archivo paises.csv se actualiza con:
+11,Chile,19107216,756102,América
+
+3. Mostrar estadísticas generales
+
+Entrada:
+=== MENÚ PRINCIPAL ===
+5
+
+Salida:
+ESTADÍSTICAS GENERALES
+País con mayor población: China (1,412,000,000 hab.)
+País con menor población: Australia (25,690,000 hab.)
+Promedio de población: 374,949,131
+Promedio de superficie: 5,337,717 km²
+Cantidad de países por continente:
+   - América: 3
+   - Europa: 2
+   - África: 2
+   - Oceanía: 1
+   - Asia: 2
+
+4. Ordenar países por población
+
+Entrada:
+
+=== MENÚ PRINCIPAL ===
+4
+Seleccione una opcion para ordenar los paises:
+1.Ordenar por nombre
+2.Ordenar por poblacion
+3.Ordenar por superficie
+0.Salir
+Elija una opcion: 2
+
+Salida (parcial):
+
+LISTA COMPLETA DE PAiSES:
+
+5 - Australia | Pob: 25,690,000 | Sup: 7,692,000 km² | Oceanía
+8 - Canadá | Pob: 38,000,000 | Sup: 9,984,670 km² | América
+1 - Argentina | Pob: 45,376,763 | Sup: 2,780,400 km² | América
+10 - Argelia | Pob: 47,400,000 | Sup: 2,381,741 km² | África
+9 - España | Pob: 47,450,795 | Sup: 505,990 km² | Europa
+3 - Alemania | Pob: 83,149,300 | Sup: 357,022 km² | Europa
+4 - Egipto | Pob: 104,000,000 | Sup: 1,002,450 km² | África
+2 - Brasil | Pob: 213,993,437 | Sup: 8,515,767 km² | América
+7 - India | Pob: 1,393,000,000 | Sup: 3,287,263 km² | Asia
+6 - China | Pob: 1,412,000,000 | Sup: 9,596,960 km² | Asia
+
+Total de paises: 10
+
+5. Filtrar países por continente
+
+Entrada:
+=== MENÚ PRINCIPAL ===
+3
+=== FILTRAR PAÍSES ===
+1. Por continente
+2. Por rango de población
+3. Por rango de superficie
+0. Volver al menú principal
+Elija una opción: 1
+Ingrese el nombre del continente: asia
+
+Salida:
+Países en el continente 'Asia':
+
+6 - China | Pob: 1,412,000,000 | Sup: 9,596,960 km² | Asia
+7 - India | Pob: 1,393,000,000 | Sup: 3,287,263 km² | Asia
+
+Total: 2 país(es)
+
+
+**Trabajo integrador final - Programación I (UTN FRM, 2025)**  
+Proyecto desarrollado con Python 3.10+ aplicando Programación Orientada a Objetos (POO).
--- integrador_primer_semestre.py
+++ integrador_primer_semestre.py
@@ -1,13 +1,10 @@
-import csv    #Sirve para leer archivos CSV (valores separados por comas)
+import csv    # Manejo de archivos CSV
 
-# Creamos una clase llamada Paises
-# Esta clase nos permite guardar toda la información de cada país de forma ordenada.
-
+# Clase que representa un país
 class Paisdelmundo:
 
-    # El método __init__ se ejecuta automaticamente cada vez que creamos un nuevo pais.
+    # Inicializa los datos de un país
     def __init__(self, codigo: int, nombre: str, poblacion: int, superficie: int, continente: str):
-        # Guardamos los datos del pais dentro del objeto
         self.codigo = codigo
         self.nombre = nombre
         self.poblacion = int(poblacion)
@@ -15,12 +12,10 @@ class Paisdelmundo:
         self.continente = continente
         
 
-    # Este metodo sirve para mostrar el pais en texto cuando usamos "print(pais)"
     def __str__(self):
-        # {:,} pone comas en los numeros grandes (ejemplo: 45,376,763)
         return f"{self.codigo} - {self.nombre} | Pob: {self.poblacion:,} | Sup: {self.superficie:,} km² | {self.continente}"
 
-    # Este metodo convierte el pais en un diccionario (util si luego queremos guardar en CSV)
+    # Convierte el país a diccionario (para guardar en CSV)
     def a_diccionario(self):
         return {
             "codigo": self.codigo,
@@ -31,7 +26,7 @@ class Paisdelmundo:
         }
 
 
-# Definimos una funcion para leer el archivo CSV
+# Lee el archivo CSV y carga los países
 def cargar_datos_csv(ruta_csv: str):
     """
     Esta función lee el archivo CSV (por ejemplo: paises.csv)
@@ -40,9 +35,8 @@ def cargar_datos_csv(ruta_csv: str):
     - un diccionario que usa el 'codigo' como clave
     """
 
-    # Creamos estructuras vacias para guardar los paises
-    lista_paises = []      # para recorrerlos o mostrar varios
-    dicc_por_codigo = {}   # para buscar rápido por código
+    lista_paises = []      
+    dicc_por_codigo = {}   
 
     try:
         # Abrimos el archivo CSV con codificación UTF-8 (para que lea acentos correctamente)
@@ -54,28 +48,23 @@ def cargar_datos_csv(ruta_csv: str):
             for fila in lector:
                 try:
                     # Convertimos los datos de texto a su tipo correcto
-                    codigo = int(fila["codigo"])              # el codigo debe ser un numero entero
-                    nombre = fila["nombre"].strip()           # quitamos espacios en blanco al principio y final
-                    poblacion = int(fila["poblacion"])        # convertimos el texto a número entero
+                    codigo = int(fila["codigo"])              
+                    nombre = fila["nombre"].strip()           
+                    poblacion = int(fila["poblacion"])       
                     superficie = int(fila["superficie"])
                     continente = fila["continente"].strip()
 
-                    # Validamos que el codigo no esté repetido
                     if codigo in dicc_por_codigo:
                         print(f"Código duplicado ({codigo}) para {nombre}, se omite.")
-                        continue  # salta al siguiente país
+                        continue  
 
-                    # Creamos el objeto Country con los datos leidos
                     pais = Paisdelmundo(codigo, nombre, poblacion, superficie, continente)
 
-                    # Agregamos el pais a la lista
                     lista_paises.append(pais)
 
-                    # También lo guardamos en el diccionario (clave = codigo)
                     dicc_por_codigo[codigo] = pais
 
                 except Exception as e:
-                    # Si hay error en esa fila (por ejemplo un dato vacío o texto donde debería haber un numero)
                     print(f"Error al procesar fila: {fila} ({e})")
 
         # Cuando terminamos de leer todo, mostramos cuantos países se cargaron
@@ -102,7 +91,6 @@ def mostrar_todos_los_paises(lista_paises: list):
 
     print("\nLISTA COMPLETA DE PAiSES:\n")
 
-    # Recorremos la lista y mostramos cada país usando __str__()
     for pais in lista_paises:
         print(pais)
 
@@ -126,7 +114,6 @@ def buscar_pais_por_nombre(lista_paises: list):
 
         opcion = input("Elija una opción: ").strip()
 
-        # salir
         if opcion == "0":
             print("Volviendo al menú principal...")
             break
@@ -282,63 +269,61 @@ def ordenar_paises_por_superficie(lista_paises: list):
     return lista_paises
 
 
-#esta funcion calcula y muestra las estadisticas  
+# Esta función calcula y muestra las estadísticas generales
+def mostrar_estadisticas(lista_paises: list):
+    """
+    Calcula:
+    - País con mayor y menor población
+    - Promedio de población y superficie
+    - Cantidad de países por continente
+    """
+    if not lista_paises:
+        print("No hay países cargados.")
+        return
 
-def mostrar_estadisticas(lista_paises:list) :
-  if not lista_paises:
-    print("no hay paises cargados")
-    return 
-  
-  #inicializamos variables para la mayor y menor poblacion 
+    # Inicializamos variables
+    pais_mayor_pob = lista_paises[0]
+    pais_menor_pob = lista_paises[0]
+    total_poblacion = 0
+    total_superficie = 0
 
-  pais_mayor_pob= lista_paises[0]
-  pais_menor_pob = lista_paises[0]
-  total_poblacion= 0
-  total_superficie=0
+    # Recorremos la lista para acumular datos
+    for p in lista_paises:
+        total_poblacion += p.poblacion
+        total_superficie += p.superficie
 
-    #Recorremos la lista
-  for p in lista_paises:
-    total_poblacion+= p.poblacion 
-    total_superficie+= p.superficie
+        if p.poblacion > pais_mayor_pob.poblacion:
+            pais_mayor_pob = p
+        if p.poblacion < pais_menor_pob.poblacion:
+            pais_menor_pob = p
 
+    # Calculamos promedios
+    promedio_pob = total_poblacion / len(lista_paises)
+    promedio_sup = total_superficie / len(lista_paises)
 
-    if p.poblacion > pais_mayor_pob.poblacion:
-      pais_mayor_pob = p
-
-    if p.poblacion < pais_menor_pob.poblacion :
-      pais_menor_pob= p
-       
-
-  promedio_pob = total_poblacion / len(lista_paises)
-  promedio_sup = total_superficie / len(lista_paises)
-
-
-    #Contar países por continente
-  paises_por_continente = {}
-  for p in lista_paises: 
+    # Contamos países por continente
+    paises_por_continente = {}
+    for p in lista_paises:
         cont = p.continente
         if cont not in paises_por_continente:
             paises_por_continente[cont] = 1
         else:
             paises_por_continente[cont] += 1
 
-  print("\n ESTADÍSTICAS GENERALES ")
-  print(f"País con mayor población: {pais_mayor_pob.nombre} ({pais_mayor_pob.poblacion:,} hab.)")
-  print(f"País con menor población: {pais_menor_pob.nombre} ({pais_menor_pob.poblacion:,} hab.)")
-  print(f"Promedio de población: {promedio_pob:,.0f}")
-  print(f"Promedio de superficie: {promedio_sup:,.0f} km²")
-  print("Cantidad de países por continente:")
-  for cont, cant in paises_por_continente.items():
+    # Mostramos resultados
+    print("\nESTADÍSTICAS GENERALES")
+    print(f"País con mayor población: {pais_mayor_pob.nombre} ({pais_mayor_pob.poblacion:,} hab.)")
+    print(f"País con menor población: {pais_menor_pob.nombre} ({pais_menor_pob.poblacion:,} hab.)")
+    print(f"Promedio de población: {promedio_pob:,.0f}")
+    print(f"Promedio de superficie: {promedio_sup:,.0f} km²")
+    print("Cantidad de países por continente:")
+    for cont, cant in paises_por_continente.items():
         print(f"   - {cont}: {cant}")
 
 
-
+# Esta función agrega un pais.
 def agregar_pais(lista_paises: list, dicc_paises: dict, ruta_csv: str):
-    """
-    Versión simple y segura de agregar_pais.
-    Genera nuevo_codigo ANTES de escribir y maneja errores sin romper el programa.
-    """
-
+    
     print("\n=== AGREGAR NUEVO PAÍS ===")
     print("(Escriba 0 y ENTER para cancelar en cualquier paso)")
 
@@ -416,9 +401,9 @@ def agregar_pais(lista_paises: list, dicc_paises: dict, ruta_csv: str):
     nuevo_pais = Paisdelmundo(nuevo_codigo, nombre, poblacion, superficie, continente)
     lista_paises.append(nuevo_pais)
     dicc_paises[nuevo_codigo] = nuevo_pais
-    guardado_en_memoria = True  #marca que ya lo pusimos en memoria
+    guardado_en_memoria = True  
 
-    #Intentar escribir en el CSV (versión simple que añade newline para evitar "pegado")
+    #Intentar escribir en el CSV 
     try:
         with open(ruta_csv, "a", encoding="utf-8") as archivo:
             #siempre escribimos un salto de linea primero para separar del último registro
